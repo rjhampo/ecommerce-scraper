a05a4e53925eb4dc561375a055e2f6aa7adeabd5
rjhampo <regiljames@yahoo.com>
2024-11-02 20:40:08 +0800

    Added documentation for base.py

--- ecommerce_scraper/scraper/base.py
+++ ecommerce_scraper/scraper/base.py
@@ -4,8 +4,17 @@ class BaseScraper:
     def __init__(self):
         pass
 
+    def _get_browser_info(self):
+        """Gets the necessary cookies and headers to 
+           help the scraper pass as a human"""
+        pass
+
     def page_scraper(self):
+        """For scraping pages from a search result; 
+           Sufficient for broad data gathering; Use item_scraper
+           for more details on entries obtained from this"""
         pass
 
     def item_scraper(self):
+        """For scraping item pages"""
         pass
