b23b13aec57e334e6a18bb72f3a40ada355ebdde
rjhampo <regiljames@yahoo.com>
2024-11-02 21:58:55 +0800

    Renamed utils.py to common.py for common functions

--- ecommerce_scraper/common.py
+++ ecommerce_scraper/common.py
@@ -3,10 +3,10 @@ from time import sleep
 from ecommerce_scraper import USER_AGENTS
 from constants import *
 
+
 def _get_user_agent():
     user_agent = choice(USER_AGENTS)
     return user_agent
-    
 
 def _get_lazada_page_headers(user_agent):
     return {
@@ -27,6 +27,6 @@ def _get_lazada_page_headers(user_agent):
 
 def _random_delay():
     if random.random() > 0.5:
-        sleep(abs(random.gauss(DELAY_MEAN, DELAY_SD)) * random.uniform(0.5,1.5))
+        sleep(abs(random.gauss(DELAY_MEAN, DELAY_STD)) * random.uniform(0.5,1.5))
     else:
-        sleep(abs(random.gauss(DELAY_MEAN, DELAY_SD)) + random.random())
+        sleep(abs(random.gauss(DELAY_MEAN, DELAY_STD)) + random.random())
