3768bf57dd02c26032466f1fbcf80f97bd7fa7c9
rjhampo <regiljames@yahoo.com>
2024-11-02 21:21:22 +0800

    Added documentation for base scraper

--- ecommerce_scraper/scraper/base.py
+++ ecommerce_scraper/scraper/base.py
@@ -2,15 +2,22 @@ from ecommerce_scraper.proxy import Proxy
 
 
 class BaseScraper:
-    def __init__(self, proxy_config: dict | None):
-        """Initialize scraper with proxy_config dict having at least 
-           one of the following items: proxy (which either contains a url string or 
-           a generator), proxy_user, and proxy_passw"""
+    def __init__(self, search_query: str, proxy_config: dict | None) -> None:
+        """Initialize scraper with following params:
+           - proxy_config dict having at least one of the following items: 
+                + proxy (which either contains a url string or a generator)
+                + proxy_user
+                + proxy_passw
+           
+           - search_query which takes the item to search e.g. soap, chips, etc.
+             This doesn't accept regular expressions
+           """
         self.proxy_config = Proxy(proxy=proxy_config.get('proxy'), proxy_user=proxy_config.get('proxy_user'),
                                   proxy_passw=proxy_config.get('proxy_passw'))
+        self.search_query = search_query
 
     def _get_browser_info(self):
-        """Gets the necessary cookies and headers to 
+        """Gets the necessary cookies, headers, and other info to 
            help the scraper pass as a human"""
         pass
 
