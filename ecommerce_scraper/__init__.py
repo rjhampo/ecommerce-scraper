706ab8f91218a463320ec72a90d0dbc1d044ae39
rjhampo <regiljames@yahoo.com>
2024-11-02 22:20:37 +0800

    Minor changes to variable naming

--- ecommerce_scraper/__init__.py
+++ ecommerce_scraper/__init__.py
@@ -1,7 +1,7 @@
 import json
 import logging
 
-logger = logging.getLogger()
+scraper_logger = logging.getLogger('__name__')
 
 with open('user_agents.json', 'r') as input:
     USER_AGENTS = json.loads(input.read())
