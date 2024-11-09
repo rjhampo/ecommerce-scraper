987d9fe35daa36f99e996669ee4b08422ec07d98
rjhampo <regiljames@yahoo.com>
2024-11-09 22:38:23 +0800

    Modified package root __init__.py
    Reason: Made a mistake in calling getLogger and reading the user_agents file

--- ecommerce_scraper/__init__.py
+++ ecommerce_scraper/__init__.py
@@ -1,7 +1,7 @@
 import json
 import logging
 
-scraper_logger = logging.getLogger('__name__')
+scraper_logger = logging.getLogger(__name__)
 
-with open('user_agents.json', 'r') as input:
+with open('ecommerce_scraper/user_agents.json', 'r') as input:
     USER_AGENTS = json.loads(input.read())
